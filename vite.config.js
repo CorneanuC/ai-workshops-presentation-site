1eb8e76d4b8aae04fd51f8eb9b8791c68bf3f5da
CORNEANU Constatin <corneanuconstantin72@gmail.com>
2026-07-26 13:54:05 +0300

    vite-config update 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5180,
+    host: '0.0.0.0',
+    port: 8080,
+    strictPort: true,
     open: false,
+    allowedHosts: ['seahorse-app-fkis4.ondigitalocean.app'],
   },
 })
